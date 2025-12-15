1b22937da4012100953f713de278fa4230942112
mjasil <nafilatk7@gmail.com>
2025-12-15 15:50:35 +0530

    commit

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,8 +1,36 @@
 "use client"
 
+import Image from "next/image"
+import { gsap } from "gsap/gsap-core";
+
+const navLinks = [
+  { label: "Home", href: "#" },
+  { label: "Courses", href: "#" },
+  { label: "Ebooks", href: "#" },
+  { label: "About", href: "#" },
+];
+
 export default function Navbar(){
   return (
-    <header className="fixed top-0 left-0">
+    <header className="fixed top-0 left-0 w-full bg-white/10 backdrop-blur-md border-b border-white/20 z-50">
+      <div className="mx=auto flex max-w-6xl items-center px-6 py-3">
+        <div className="h-13 w-10 overflow-hidden">
+          <Image
+          src="/logo.svg"
+          alt="site logo "
+          width={40}
+          height={40}
+          className="h-13 w-10 object-contain"
+          priority
+          />
+
+        </div>
+
+        <div>
+          
+        </div>
+      
+      </div>
 
     </header>
   )
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "./components/Navbar";
 import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +31,7 @@ export default function RootLayout({
       >
         <Navbar/>
         {children}
-        <Footer/>
+        {/* <Footer/> */}
       </body>
     </html>
   );
